d0c90b0db0b3620d5a42cb3cfe134538302cf022
bbugga01 <bbugga01@naver.com>
2020-05-17 02:58:05 +0900

    영화 목록(best 50) 크롤링

---
+++ crolling/src/crolling/movie.java
@@ -0,0 +1,26 @@
+package crolling;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class movie {
+	public static void main(String[] args) throws IOException{
+		
+		String url = "https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200515";
+				
+		Document document = Jsoup.connect(url).get();
+		Elements movies = document.select("div.tit5");
+		
+		//System.out.println(movies);
+		for (Element movie : movies){
+			// System.out.println(movie.select("a"));
+			System.out.println(movie.select("a").attr("href"));
+			System.out.println(movie.text());
+			
+		}
+	}
+}
